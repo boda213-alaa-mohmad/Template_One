762557b408c10c2f19e30dd84f3e33514e37480e
boda213-alaa-mohmad <alaaabdelrhman213@gmail.com>
2026-03-15 14:47:55 +0200

    starting project setting

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "elzerowebschool.github.io",
+      },
+      {
+        protocol: "https",
+        hostname: "w3schools.com",
+      },
+      {
+        protocol: "https",
+        hostname: "thumbs.dreamstime.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
