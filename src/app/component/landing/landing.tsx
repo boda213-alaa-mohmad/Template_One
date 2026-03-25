03e06f2b73a69f3dd0196e3b963e664983898bea
boda213-alaa-mohmad <alaaabdelrhman213@gmail.com>
2026-03-25 07:42:49 +0200

    creating landing image with text

---
+++ src/app/component/landing/landing.tsx
@@ -0,0 +1,11 @@
+import styles from './landing.module.css'
+export default function Landing() {
+    return (
+      <div className={styles.landing}>
+        <div className={styles.container}>
+          <h2>Hello There</h2>
+          <p>We are Leon - Super Creative & Minimal Agency Web Template</p>
+        </div>
+      </div>
+    );
+}
